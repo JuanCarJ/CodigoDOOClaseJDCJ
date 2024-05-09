9b86bafc996937394d726508928651c3f0f87ff4
Juan Cardenas <juan.cardenas5279@uco.net.co>
2024-05-08 20:23:38 -0500

    Actualizacion Clase 0805

--- TiendaChepito/src/main/java/co/edu/uco/tiendachepito/data/dao/sql/azuresql/PaisAzureSqlDAO.java
+++ TiendaChepito/src/main/java/co/edu/uco/tiendachepito/data/dao/sql/azuresql/PaisAzureSqlDAO.java
@@ -1,8 +1,13 @@
 package co.edu.uco.tiendachepito.data.dao.sql.azuresql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import co.edu.uco.tiendachepito.crosscutting.exceptions.custom.DataTiendaChepitoException;
 import co.edu.uco.tiendachepito.data.dao.PaisDAO;
 import co.edu.uco.tiendachepito.data.dao.sql.SqlConnection;
 import co.edu.uco.tiendachepito.entity.PaisEntity;
@@ -17,9 +22,19 @@ public final class PaisAzureSqlDAO extends SqlConnection implements PaisDAO {
 	public final void crear(final PaisEntity entidad) {
 		final var sentenciaSql = new StringBuilder();
 		
-		sentenciaSql.append("INSERT INTO Pais(Nombre)");
-		sentenciaSql.append("VALUES('Colombia')");
+		sentenciaSql.append("INSERT INTO Pais(Nombre) ");
+		sentenciaSql.append("VALUES(?)");
+		try(final PreparedStatement sentenciaPreparada = getConnection().prepareStatement(sentenciaSql.toString())){
+			sentenciaPreparada.setString(1,  entidad.getNombre());
+			sentenciaPreparada.executeUpdate();
 
+		}catch (final SQLException exception){
+			var mensajeUsuario = "No ha sido posible llevar a cabo el registro de la inofrmacion del nuevo Pais. Por favor intente de neuvo y en caso de persistir el problema, comuniquese con el administrador de la tienda chepito";
+			var mensajeTecnico = "Se ha presentado un problema tratando de realizar un insert de la informacion del pais";
+			throw  new DataTiendaChepitoException(mensajeTecnico, mensajeUsuario, exception);
+		}catch (final Exception exception){
+
+		}
 	}
 
 	@Override
@@ -42,14 +57,29 @@ public final class PaisAzureSqlDAO extends SqlConnection implements PaisDAO {
 
 	@Override
 	public final List<PaisEntity> consultar(PaisEntity entidad) {
-
+		final var listaPaises = new ArrayList<PaisEntity>();
 		final var sentenciaSql = new StringBuilder();
 		
 		sentenciaSql.append("SELECT id, nombre");
 		sentenciaSql.append("FROM Pais");
 		sentenciaSql.append("ORDER BY nombre ASC");
-		
-		return null;
+
+		try(final PreparedStatement sentenciaPreparada = getConnection().prepareStatement(sentenciaSql.toString())){
+			try(final ResultSet resultados = sentenciaPreparada.executeQuery()){
+				 	while ((resultados.next())){
+						 PaisEntity paisTmp = PaisEntity.build(resultados.getInt("id"), resultados.getString("nombre"));
+						 listaPaises.add(paisTmp);
+
+					}
+			}
+
+		}catch (final SQLException exception){
+			var mensajeUsuario = "No ha sido posible llevar a cabo el registro de la inofrmacion del nuevo Pais. Por favor intente de neuvo y en caso de persistir el problema, comuniquese con el administrador de la tienda chepito";
+			var mensajeTecnico = "Se ha presentado un problema tratando de realizar un insert de la informacion del pais";
+			throw  new DataTiendaChepitoException(mensajeTecnico, mensajeUsuario, exception);
+		}catch (final Exception exception){
+
+		}return listaPaises;
 	}
 
 }
